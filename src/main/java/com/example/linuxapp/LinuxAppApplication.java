0f5647589eb51b3146441dfdf88f28bd99f35a10
Sreejith S <sreejithsev@gmail.com>
2022-08-06 12:35:27 +0530

    Initial commit

---
+++ src/main/java/com/example/linuxapp/LinuxAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.linuxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinuxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linuxapp/LinuxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linuxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
